71941a5fe9dd7d388a9383dc24c179cbe73a4301
kzrepo <kz.sendme@gmail.com>
2022-03-17 05:37:01 +0100

    untracked files on bm3-obsluga-bazy-danych: 8767ed3 refactor: wydziel baze danych do oddzielnego projektu

---
+++ Firma.Data/Data/Sklep/Rodzaj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma.Data.Data.Sklep
+{
+    public   class Rodzaj
+    {
+        [Key]
+        public int IdRodzaju { get; set; }
+        
+        [Required(ErrorMessage = "Wpisz nazwę rodzaju")]
+        [MaxLength(20, ErrorMessage = "Rodzaj powinien zawierać max 20 znaków")]
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+    }
+}
